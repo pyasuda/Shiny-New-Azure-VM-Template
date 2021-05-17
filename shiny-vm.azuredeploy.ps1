801c348f04b344355ed9a949de3a921d694e0df7
pyasuda <pyasuda@getadvantage.com>
2021-05-17 13:32:42 -0400

    Updated the files for shiny new VM

--- shiny-vm.azuredeploy.ps1
+++ shiny-vm.azuredeploy.ps1
@@ -1,5 +1,5 @@
 # The vnet in the parameters file must be in the same resource group as the VM
 New-AzResourceGroupDeployment `
 -ResourceGroupName rg-AzureMigrateTest-mpn-001 `
--TemplateFile .\AIHITS-PROD-WEB1.azuredeploy.json `
--TemplateParameterFile .\AIHITS-PROD-WEB1.azuredeploy.parameters.json
+-TemplateFile .\shiny-vm.azuredeploy.json `
+-TemplateParameterFile .\shiny-vm.azuredeploy.parameters.json
